6a03017348516d28801e8a30d59e0126b7a07d74
MoisesAzevedo <MoisesDev2022@gmail.com>
2025-08-27 17:56:35 -0300

    overflow table

--- src/pages/Orders/local-components/OrderItemsTable/components/AdminArticleActions.tsx
+++ src/pages/Orders/local-components/OrderItemsTable/components/AdminArticleActions.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ExternalLinkIcon } from './Icons';
-import { AdminDropdownMenu } from './AdminDropdownMenu';
+import { PencilIcon } from '../../../../../icons';
 import { openExternalLink } from '../utils/fileActions';
 
 interface AdminArticleActionsProps {
@@ -20,6 +20,8 @@ export const AdminArticleActions: React.FC<AdminArticleActionsProps> = ({
     openExternalLink(url);
   };
 
+  const iconSizeClass = iconSize === 'small' ? 'w-3 h-3' : iconSize === 'large' ? 'w-5 h-5' : 'w-4 h-4';
+
   return (
     <div className={`flex items-center gap-0.5 ${className}`}>
       {/* External Link Icon */}
@@ -31,12 +33,14 @@ export const AdminArticleActions: React.FC<AdminArticleActionsProps> = ({
         <ExternalLinkIcon size={iconSize} />
       </button>
 
-      {/* Admin Dropdown Menu */}
-      <AdminDropdownMenu
-        url={url}
-        onEdit={onEdit}
-        iconSize={iconSize}
-      />
+      {/* Edit Button - Only for Admin */}
+      <button
+        onClick={onEdit}
+        className="p-1 hover:bg-gray-100 dark:hover:bg-gray-700 rounded transition-colors"
+        title="Editar URL"
+      >
+        <PencilIcon className={`${iconSizeClass} text-gray-500 hover:text-brand-500`} />
+      </button>
     </div>
   );
 };
